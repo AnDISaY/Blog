86835ec50daf22249224c69aec6c7805ea089344
AnDISaY <sainirgg@inbox.ru>
2021-12-21 18:28:55 +0600

    Add account/models/

--- blog/settings.py
+++ blog/settings.py
@@ -39,7 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # '',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_yasg',
+    'django_filters',
+
+    'account',
+    'posts',
 ]
 
 MIDDLEWARE = [
