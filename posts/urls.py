ba857c309b9ba4668c03d0ede8ab0b2522b610d7
AnDISaY <sainirgg@inbox.ru>
2021-12-26 13:48:30 +0600

    Change likes

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
 from .views import (PostViewSet, TagViewSet,
-                    CommentViewSet, FavoritesListView, FavoritesCreateView, FavoritesDestroyView, PostLikeViewSet)
+                    CommentViewSet, FavoritesListView, FavoritesCreateView, FavoritesDestroyView)
 
 
 router = SimpleRouter()
@@ -10,7 +10,7 @@ router.register('post', PostViewSet)
 router.register('tags', TagViewSet)
 router.register('comments', CommentViewSet)
 # router.register('favorites_list', FavoritesListView)
-router.register('post_likes', PostLikeViewSet)
+# router.register('post_likes', LikeViewSet)
 
 
 urlpatterns = [
@@ -18,4 +18,7 @@ urlpatterns = [
     path('favorites/', FavoritesListView.as_view()),
     path('favorites/add/', FavoritesCreateView.as_view()),
     path('favorites/delete/<int:pk>/', FavoritesDestroyView.as_view()),
+    # path('post/<int:pk>/add/', LikeCreateView.as_view()),
+    # path('post/<int:pk>/remove/', LikeDestroyView.as_view()),
+
 ]
